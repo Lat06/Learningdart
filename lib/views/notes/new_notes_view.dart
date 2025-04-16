1862cb986d6e4396decf6c4b9da6ccae6aa32866
Lat06 <theusername06@gmail.com>
2025-04-16 11:25:55 +0300

    step-13

---
+++ lib/views/notes/new_notes_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NewNoteView extends StatefulWidget {
+  const NewNoteView({super.key});
+
+  @override
+  State<NewNoteView> createState() => _NewNotesViewState();
+}
+
+class _NewNotesViewState extends State<NewNoteView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New note'),
+      ),
+      body: const Text('Write your new note here...'),
+    );
+  }
+}
